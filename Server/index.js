2e3d75f7d6921352fb5bb8b9ca02cf28236e7a94
Karan Kumar <officialkaran39@gmail.com>
2024-07-15 00:27:33 +0530

    node js

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,8 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
+
 
 const myServer = http.createServer((req, res) => {
     //console.log("New Request");
@@ -7,18 +10,32 @@ const myServer = http.createServer((req, res) => {
     //res.end("Hello from server");
 
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
         //res.end("Hello from server");
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case "/":
                 res.end("Hello from server");
                 break;
             case "/about":
-                res.end("About from server");
+                const username = myUrl.query.username;
+                res.end(`Hi, ${username} from server`);
                 break;
             case "/contact":
-                res.end("Contact from server");
+                const contact = myUrl.query.contact;
+                res.end(`Hello, ${contact} from server`);
                 break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end(`Hello, ${search} from server`);
+                break;
+            case "/signup":
+                if(req.method === "GET") res.end("This is a signup form");
+                else if (req.method === "POST"){
+                    // db query
+                    res.end("success");
+                }
             default:
                 res.end("404 from server");
                 break;
@@ -30,3 +47,10 @@ const myServer = http.createServer((req, res) => {
 myServer.listen(8000, () => {
     console.log("Server is listening on port 8000");
 })
+
+
+
+
+
+// Http get - when u want to get some data from the server
+// http post - when u want to send and mutate some data in server
